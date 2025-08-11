7e271210349da4757c50fcf0cef3fb1350df25ba
Nicolas Castillo <121370448+iconicastill@users.noreply.github.com>
2025-08-11 15:07:56 -0400

    Create urls.js

---
+++ urls.js
@@ -0,0 +1,4 @@
+const urlInstagram = "https://www.instagram.com/iconicastill/";
+const enlace = document.getElementById("enlace-a-instagram");
+enlace.href = urlInstagram;
+enlace.target = "_blank";
