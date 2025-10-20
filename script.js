8b192bb7cb0633a58a439712e59975c8fe9f1039
Nicolas Castillo <121370448+iconicastill@users.noreply.github.com>
2025-10-19 22:27:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -73,3 +73,13 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+// Contador de textarea
+
+  const textarea = document.getElementById("mensaje");
+  const usados = document.getElementById("usados");
+
+  textarea.addEventListener("input", () => {
+    usados.textContent = textarea.value.length;
+  });
+
+
